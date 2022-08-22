79a28b72b30bba6de8789b7ec5c9d29f6c713059
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-08-22 02:14:21 -0300

    Service Discovery com Eureka

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.alura.microservice.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,11 +12,13 @@ import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
 @Service
 public class CompraService {
 
+	@Autowired
+	private RestTemplate client;
+	
 	public void realizaCompra(CompraDTO compra) {
 		
-		RestTemplate client = new RestTemplate();
 		ResponseEntity<InfoFornecedorDTO> exchange = 
-			client.exchange("http://localhost:8081/info/"+compra.getEndereco().getEstado(),
+			client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(),
 			HttpMethod.GET, null, InfoFornecedorDTO.class);
 		
 		System.out.println(exchange.getBody().getEndereco());
